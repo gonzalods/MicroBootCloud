cfaced9a07d3bef1ee7b57544c3017aae3b74bb1
0003299 <gms@mail>
2016-06-23 11:57:06 +0200

    Propiedades configuracion -> yaml

--- micro-chasis/src/test/java/org/gms/microservice/MyMicroservice.java
+++ micro-chasis/src/test/java/org/gms/microservice/MyMicroservice.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Produces;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import jersey.repackaged.com.google.common.collect.ImmutableMap;
@@ -34,13 +35,17 @@ public class MyMicroservice extends Microservice {
 	public static class DumbResource{
 		
 		@Autowired
+		private Environment env;
+		
+		@Autowired
 		public DumbResource(JerseyConfig jerseyConfig){
 			jerseyConfig.register(DumbResource.class);
 		}
 		@GET
 		@Produces("application/json")
-		public Map<String, Boolean> getResource(){
-			return ImmutableMap.of("value", true);
+		public Map<String, String> getResource(){
+			return ImmutableMap.of("sample.config", env.getProperty("sample.config"),
+					"microservice.name", env.getProperty("spring.application.name"));
 		}
 	}
 	
